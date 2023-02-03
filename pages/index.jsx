0c3aeeba87a6f91d575a438e418f78397c1a88e8
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-02-03 23:02:32 +1300

    🔥 Remove unnecessary import

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import { Navigation } from '@/components/Navigation';
 import Link from 'next/link';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
