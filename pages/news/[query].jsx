41149d3ac5c1cebec9db715826a392acc5f1bb2f
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-02-04 22:31:29 +1300

    ✨ Implement setServerSidePRops

--- pages/news/[query].jsx
+++ pages/news/[query].jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import { search } from '../api';
 
-const News = () => {
+const News = ({ results, query }) => {
   return (
     <div>
-      <h1>Search result</h1>
+      <h1>Search results for: {query}</h1>
+      <ul>
+        {results.map((result) => (
+          <li key={result.url}>
+            <a href={result.url} target='_blank' rel='noreferrer'>
+              {result.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default News;
 
-export async function getServerSideProps() {
-  // const API_KEY = process.env.NEW_YORK_TIMES_API_KEY;
-  // const URL = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`;
-  // const res = await fetch(URL);
-  // const data = await res.json();
+export async function getServerSideProps({ params }) {
+  const API_KEY = process.env.NEW_YORK_TIMES_API_KEY;
+  const URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${params.query}&api-key=${API_KEY}`;
+  const results = await search(URL);
   return {
-    props: {},
+    props: {
+      results,
+      query: params.query,
+    },
   };
 }
