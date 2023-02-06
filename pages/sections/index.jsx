75074e6ac9ed638a557515e039c8925a7215d417
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-02-06 22:00:05 +1300

    ✨ Add section list page and single section page

---
+++ pages/sections/index.jsx
@@ -0,0 +1,30 @@
+import { handler } from '../api';
+
+const Sections = ({ results }) => {
+  return (
+    <div>
+      <h1>Sections</h1>
+      <ul>
+        {results.map((result) => (
+          <li key={`sections/${result.section}`}>
+            <a href={`sections/${result.section}`}>{result.display_name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sections;
+
+export async function getStaticProps() {
+  const API_KEY = process.env.NEW_YORK_TIMES_API_KEY;
+  const URL = `https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${API_KEY}`;
+  const results = await handler(URL);
+
+  return {
+    props: {
+      results,
+    },
+  };
+}
