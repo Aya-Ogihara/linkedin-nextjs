11eb4a04226a1dafedead954df83233de73f029d
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-02-06 14:10:02 +1300

    ✨ Add custom 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,13 @@
+import { GoBack } from '@/components/GoBack';
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <main className='main'>
+      <h1>Page Not Found</h1>
+      <GoBack />
+    </main>
+  );
+};
+
+export default NotFound;
